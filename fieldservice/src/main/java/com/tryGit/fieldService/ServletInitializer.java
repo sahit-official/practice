1f0518279058b98ea260ba888ace40abafa0435b
Adithya Narayansamal <adithya.narayansamal@kumaran.com>
2025-06-23 14:25:40 +0530

    added stuff

---
+++ fieldservice/src/main/java/com/tryGit/fieldService/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tryGit.fieldService;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FieldServiceApplication.class);
+	}
+
+}
---
+++ fieldservice/src/test/java/com/tryGit/fieldService/FieldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryGit.fieldService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
