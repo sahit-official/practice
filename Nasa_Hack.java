88aec6c4933ca5e7af9cd84557bb69f872bbff5a
Aryan Chaudhary <aryan.chaudhary@kumaran.com>
2025-06-23 12:52:16 +0530

    tspmosahitpp

---
+++ Nasa_Hack.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+
+class Nasa_hack{
+
+
+    public static void main(String [] args){
+
+        int n=5;
+        while(n>0){
+
+            System.out.println("Welcome to product team");
+        }
+    }
+}
